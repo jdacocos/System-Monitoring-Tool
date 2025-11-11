66b44122af8de91704d88d295fdb06444d85ad44
tarun jaswal <tjasw549@mtroyal.ca>
2025-11-11 20:01:37 +0000

    added cpu usage

--- cpu.py
+++ cpu.py
@@ -1,1 +1,24 @@
 # Get CPU information 
+
+import time
+import os
+
+def read_file(path):
+    fd = os.open(path, os.O_RDONLY)
+
+    #python handles buffer unlike in C
+    data = os.read(fd, 4096).decode()
+    os.close(fd)
+    return data
+
+def get_cpu_times():
+    content = read_file("/proc/stat")
+    values = list(map(int, content.split("\n")[0].split()[1:]))
+    idle = values[3] + values[4]
+    return idle, sum(values)
+
+def get_cpu_usage():
+    idle1, total1 = get_cpu_times()
+    time.sleep(0.1)
+    idle2, total2 = get_cpu_times()
+    return round(100 * (1 - (idle2 - idle1) / (total2 - total1)), 2)
--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 # File to test modules independently
+
+import os
+from cpu import get_cpu_usage
+
+print("PID: ", os.getpid())
+print("CPU:", get_cpu_usage(), "%")
+
+
+# Testing
+import psutil
+print("psutil CPU:", psutil.cpu_percent(interval=0.1), "%")
