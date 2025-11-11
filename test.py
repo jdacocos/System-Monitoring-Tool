b87caa909c6cd4f42da0e7f6ebaef64b792cef45
Tarun Jaswal <147879459+tjasw549@users.noreply.github.com>
2025-11-11 13:04:21 -0700

    Formatting fix for CPU utilization

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import os
 from cpu import get_cpu_usage
 
 print("PID: ", os.getpid())
-print("CPU:", get_cpu_usage(), "%")
+print("CPU: ", get_cpu_usage(), "%")
 
 
 # Testing
 import psutil
-print("psutil CPU:", psutil.cpu_percent(interval=0.1), "%")
+print("psutil CPU: ", psutil.cpu_percent(interval=0.1), "%")
