e5e1e2df52b493e9c18bc64172acb52c0f503876
Mack Bautista <mbaut981@mtroyal.ca>
2025-11-19 20:54:22 +0000

    Working on retreiving process CPU_percent
    So far I have created two helper functions which will be called by get_process_cpu_percent eventually

--- process_util.py
+++ process_util.py
@@ -2,6 +2,7 @@ from process_struct import ProcessInfo
 
 
 import os
+import time
 from typing import Iterator
 
 LNX_FS = '/proc'
@@ -76,3 +77,40 @@ def get_process_user(pid: int) -> str | None:
 
     return username
     
+def _read_proc_stat_total() -> int:
+    """
+    Helper: Retrieves total kernel time unit.
+    Returns total CPU jiffies from /proc/stat
+    Jiffy - basic unit of time Linux kernel uses to measure CPU activity.
+    """
+
+    total_jiffies = 0
+    
+    try:
+        with open("/proc/stat", "r") as f:
+            line = f.readline()
+            if line.startswith("cpu "):
+                fields = line.split()[1:] # skip cpu column
+                total_jiffies = sum(int(f) for f in fields[:4])
+    except FileNotFoundError:
+        pass
+
+    return total_jiffies
+
+def _read_proc_pid_time(pid: int) -> int:
+    """
+    Helper: 
+    Returns total CPU jiffies used by a process (utime + stime) by reading
+    /proc/<pid>/stat.
+    """
+
+    total_proc_jiffies = 0
+
+    stat_path = f"/proc/{pid}/stat"
+
+    try:
+        with open(stat_path, "r") as f:
+            fields = f.read().split()
+            print(fields)
+    except (FileNotFoundError, IndexError, ValueError):
+        pass
